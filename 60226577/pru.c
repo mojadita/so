093bff6eac4c8c1f7152ccb023a8a9955c55d7db
Luis Colorado <luiscoloradourcola@gmail.com>
2020-02-15 13:20:44 +0200

    Detab pru.c
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- 60226577/pru.c
+++ 60226577/pru.c
@@ -1,37 +1,37 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 pid_t my_fork()
 {
-	pid_t chld = fork();
-	if (chld > 0) {
-		/* only the parent shows this */
-		printf("[pid=%d, child=%d] forked!\n",
-			getpid(), chld);
-	}
-	return chld;
+    pid_t chld = fork();
+    if (chld > 0) {
+        /* only the parent shows this */
+        printf("[pid=%d, child=%d] forked!\n",
+            getpid(), chld);
+    }
+    return chld;
 }
 
-int main() 
-{ 
-	printf("[pid=%d] Hello, world\n", getpid());
+int main()
+{
+    printf("[pid=%d] Hello, world\n", getpid());
 
-    if (my_fork() && (!my_fork())) { 
+    if (my_fork() && (!my_fork())) {
         if (my_fork() || my_fork()) {
-            my_fork(); 
-        } 
-    } 
+            my_fork();
+        }
+    }
 
-	pid_t pid;
-	int status;
+    pid_t pid;
+    int status;
 
-	while((pid = wait(&status)) >= 0) {
-    	printf("[pid=%d, child=%d]: child ended with status=%d\n",
-			getpid(), pid, status);
-	}
-	printf("[pid=%d]: Hello friend!\n", getpid());
+    while((pid = wait(&status)) >= 0) {
+        printf("[pid=%d, child=%d]: child ended with status=%d\n",
+            getpid(), pid, status);
+    }
+    printf("[pid=%d]: Hello friend!\n", getpid());
 
-    return 0; 
+    return 0;
 }
