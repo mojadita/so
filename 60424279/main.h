2842107cc709c4bcff7b0852c80340448ced1ed2
Luis Colorado <luiscoloradourcola@gmail.com>
2020-02-29 02:17:00 +0200

    Detabbed
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- 60424279/main.h
+++ 60424279/main.h
@@ -8,42 +8,42 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#define BUFSIZE				1024
+#define BUFSIZE             1024
 
 #define F(_fmt) __FILE__":%d:%s: " _fmt, __LINE__, __func__
 
-#define ERR(_fmt, ...) do {							\
-		fprintf(stderr,								\
-			F("ERROR: " _fmt), ##__VA_ARGS__);		\
-	} while (0)
+#define ERR(_fmt, ...) do {                         \
+        fprintf(stderr,                             \
+            F("ERROR: " _fmt), ##__VA_ARGS__);      \
+    } while (0)
 
-#define WARN(_fmt, ...) do {						\
-		fprintf(stderr,								\
-			F("WARN: "_fmt), ##__VA_ARGS__);		\
-	} while (0)
+#define WARN(_fmt, ...) do {                        \
+        fprintf(stderr,                             \
+            F("WARN: "_fmt), ##__VA_ARGS__);        \
+    } while (0)
 
-#define INFO(_fmt, ...) do {						\
-		fprintf(stderr,								\
-			F("INFO: "_fmt), ##__VA_ARGS__);		\
-	} while (0)
+#define INFO(_fmt, ...) do {                        \
+        fprintf(stderr,                             \
+            F("INFO: "_fmt), ##__VA_ARGS__);        \
+    } while (0)
 
-#define DEB(_fmt, ...) do {							\
-		if (flags & FLAG_VERBOSE)					\
-			fprintf(stderr,							\
-				F("LOG: " _fmt), ##__VA_ARGS__);	\
-	} while (0)
+#define DEB(_fmt, ...) do {                         \
+        if (flags & FLAG_VERBOSE)                   \
+            fprintf(stderr,                         \
+                F("LOG: " _fmt), ##__VA_ARGS__);    \
+    } while (0)
 
 
-#define FLAG_HELP			(1 << 0)
-#define FLAG_FILES			(1 << 1)
-#define FLAG_INVALID		(1 << 2)
-#define FLAG_QUIET			(1 << 3)
-#define FLAG_SUMMARY		(1 << 4)
-#define FLAG_VERBOSE		(1 << 5)
+#define FLAG_HELP           (1 << 0)
+#define FLAG_FILES          (1 << 1)
+#define FLAG_INVALID        (1 << 2)
+#define FLAG_QUIET          (1 << 3)
+#define FLAG_SUMMARY        (1 << 4)
+#define FLAG_VERBOSE        (1 << 5)
 
-#define EXIT_HELP			(1 << 0)
-#define EXIT_EMALLOC		(1 << 1)
-#define EXIT_EOPEN			(1 << 2)
+#define EXIT_HELP           (1 << 0)
+#define EXIT_EMALLOC        (1 << 1)
+#define EXIT_EOPEN          (1 << 2)
 
 
 extern int flags;
